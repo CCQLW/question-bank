189f2b9687528c1b72ba9fb1f3b24e7a3f105a27
Q <88545157+CCQLW@users.noreply.github.com>
2021-10-10 16:26:17 +0800

    Create A.cpp

---
+++ Codeforces/747/A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+ 
+void solve(){
+    ll n;
+    scanf("%lld",&n);
+    printf("%lld %lld\n", (n-1) * -1, n );
+}
+ 
+int main(){
+    int T;
+    scanf("%d", &T);
+    while (T -- ) solve();
+    // system("pause");
+}
